8265d474c957fc7f7df9153d53e13038534dfac0
ConxiCr <conxicr@hotmail.com>
2021-06-07 16:33:24 +0200

    configuración rutas
    Signed-off-by: ConxiCr <conxicr@hotmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,17 @@ import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
 
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     PaisModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
